abce11afb56d2a0c369c4d8baa25a5653507d3ac
Seetharam321 <110546532+Seetharam321@users.noreply.github.com>
2025-02-04 13:31:57 +0530

    Add files via upload

---
+++ dags/Task Groups.py
@@ -0,0 +1,35 @@
+#Task Groups
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.task_group import TaskGroup
+from datetime import datetime
+
+dag = DAG('task_group',
+    description='Writing DAG using TaskGroups to organize tasks',
+    schedule_interval='@daily',
+    start_date=datetime(2025, 2, 2),
+    catchup=False
+)
+
+start_task = DummyOperator(
+    task_id='start_task',
+    dag=dag
+)
+
+end_task = DummyOperator(
+    task_id='end_task',
+    dag=dag
+)
+
+with TaskGroup("data_processing", dag=dag) as data_processing:
+    task_A = DummyOperator(task_id="task_A")
+    task_B = DummyOperator(task_id="task_B")
+    task_C = DummyOperator(task_id="task_C")
+
+with TaskGroup("data_validation", dag=dag) as data_validation:
+    task_X = DummyOperator(task_id="task_X")
+    task_Y = DummyOperator(task_id="task_Y")
+
+start_task >> data_processing >> data_validation >> end_task
+task_A >> task_B >> task_C
+task_X >> task_Y

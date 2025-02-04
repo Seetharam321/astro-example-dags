025ee22d9c99a8b318ad995c5de46e2c6e653c40
Seetharam321 <110546532+Seetharam321@users.noreply.github.com>
2025-02-04 12:55:02 +0530

    Update Sensors-File Sensor.py

--- dags/Sensors-File Sensor.py
+++ dags/Sensors-File Sensor.py
@@ -1,18 +1,17 @@
-#Sensors-File Sensor
 from airflow import DAG
 from airflow.operators.sensors import FileSensor
 from datetime import datetime
 
 dag = DAG('file_sensor',
-    description='DAG using FileSensor(To wait for file arrival)',
+    description='DAG using FileSensor (To wait for file arrival)',
     schedule_interval='@daily',
-    start_date=datetime(2025, 2, 2),
+    start_date=datetime(2025, 2, 4),
     catchup=False
 )
 
 wait_for_file = FileSensor(
     task_id='wait_for_file',
-    filepath='C:\Users\saiseetharam.k\Downloads\012- Python-SQS-Lambda-Dynamodb.txt',
+    filepath='/mnt/data/012-Python-SQS-Lambda-Dynamodb.txt',  # Change to a Unix-style path
     poke_interval=60,
     timeout=600,
     mode='poke',

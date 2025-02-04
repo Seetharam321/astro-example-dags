b1f8446edffef804df694515969a772bc7127c87
Seetharam321 <110546532+Seetharam321@users.noreply.github.com>
2025-02-04 16:19:04 +0530

    Update Connections 101.py

--- dags/Connections 101.py
+++ dags/Connections 101.py
@@ -1,19 +1,19 @@
-#Connections 101
 from airflow import DAG
-from airflow.operators.postgres_operator import PostgresOperator
-from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 def fetch_data_from_postgres():
     postgres_hook = PostgresHook(postgres_conn_id='my_postgres_conn')
-    sql = "SELECT * FROM my_table LIMIT 10;"
+    sql = "SELECT * FROM my_table LIMIT 13;"
     result = postgres_hook.get_records(sql)
     print(result)
 
 dag = DAG(
     'connections_example',
     description='Connection in DAG',
-    schedule_interval='@daily',
+    schedule='@daily',  # Use "schedule" instead of "schedule_interval"
     start_date=datetime(2025, 2, 2),
     catchup=False
 )

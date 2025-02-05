6d80da09098549fe0fcd0421eedde9bf85e0a5fd
Seetharam321 <110546532+Seetharam321@users.noreply.github.com>
2025-02-05 13:38:27 +0530

    Add files via upload

--- dags/DAG with Parallel Dependencies(Tasks).py
+++ dags/DAG with Parallel Dependencies(Tasks).py
@@ -1,46 +1,31 @@
-#DAG with Parallel Dependencies(Tasks)
 from airflow import DAG
-from airflow.operators.python  import PythonOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-def extract():
-    print("Extraction of data should be done")
+def extract_data():
+    print("Extracting data...")
 
-def clean():
-    print("Cleaning of the extracted data")
+def clean_data():
+    print("Cleaning data...")
 
-def transform():
-    print("Transforming the cleaned data")
+def transform_data():
+    print("Transforming data...")
 
-def load():
-    print("Loading the transformed data")
+def load_data():
+    print("Loading data...")
 
-with DAG("parallel_dependencies",
+with DAG(
+    "parallel_dependencies_1_30pm_to_4_30pm",
     schedule_interval="30 13 5 2 *",
     start_date=datetime(2025, 2, 5, 13, 30),
     end_date=datetime(2025, 2, 5, 16, 30),
     catchup=False
 ) as dag:
-
-    extract_task = PythonOperator(
-        task_id="extract_data",
-        python_callable=extract_data
-    )
-
-    clean_task = PythonOperator(
-        task_id="clean_data",
-        python_callable=clean_data
-    )
-
-    transform_task = PythonOperator(
-        task_id="transform_data",
-        python_callable=transform_data
-    )
-
-    load_task = PythonOperator(
-        task_id="load_data",
-        python_callable=load_data
-    )
-
+    
+    extract_task = PythonOperator(task_id="extract_data", python_callable=extract_data)
+    clean_task = PythonOperator(task_id="clean_data", python_callable=clean_data)
+    transform_task = PythonOperator(task_id="transform_data", python_callable=transform_data)
+    load_task = PythonOperator(task_id="load_data", python_callable=load_data)
+    
     extract_task >> [clean_task, transform_task]
-    [clean_task, transform_task] >> load_task   
+    [clean_task, transform_task] >> load_task

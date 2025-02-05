198f810e0f8f429224c76531d5321ca26d009179
Seetharam321 <110546532+Seetharam321@users.noreply.github.com>
2025-02-05 18:05:21 +0530

    Add files via upload

---
+++ dags/VARIABLES_101.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+from datetime import datetime
+
+def print_variables():
+    var1 = Variable.get("my_variable1", default_var="default_value1")
+    var2 = Variable.get("my_variable2", default_var="default_value2")
+    print(f"Variable 1: {var1}, Variable 2: {var2}")
+
+with DAG(
+    "VARIABLES_101",
+    start_date=datetime(2025, 2, 5, 12, 30),
+    schedule_interval="@once",
+    catchup=False
+) as dag:
+
+    print_task = PythonOperator(
+        task_id="print_variables",
+        python_callable=print_variables
+    )
+
+    print_task
